dc58c1a402ab3c0dbe4f55e66148169469c1da45
samsul7718 <samsul7718@gmail.com>
2025-03-09 00:38:54 +0530

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-// import ToDo from "./ToDo";
+import ToDo from "./ToDo";
 
 function App() {
   return (
     <>
-      <div>{/* <ToDo /> */}</div>
+      <div>
+        <ToDo />{" "}
+      </div>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import ToDo from "./ToDo.jsx";
+// import ToDo from "./ToDo.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ToDo />
+    <App />
   </StrictMode>
 );
