604ada80010f05df18f19b6687f81f37f863279a
samsul alam <samsul7718@gmail.com>
2025-06-30 23:16:21 +0530

    line 27 updated

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -24,7 +24,7 @@ function ToDo() {
     if (inputData.trim() !== "") {
       setNewItem((prev) => [...prev, { text: inputData, checked: false }]);
       setInputData("");
-      console.log("stp num 3 ",inputData);
+      console.log("step num 3 ",inputData);
       
     }
   };
